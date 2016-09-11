fe7bfe8a3ee06eb4c7811b71b008a2950264edc6
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-11 19:59:42 -0300

    applying style sheet on background.java

--- src/pitzik4/ageOfTheInternet/graphics/Background.java
+++ src/pitzik4/ageOfTheInternet/graphics/Background.java
@@ -6,27 +6,28 @@ import java.awt.image.BufferedImage;
 import pitzik4.ageOfTheInternet.Tickable;
 
 public class Background implements Renderable, Tickable {
-	private int x=0, y=0;
-	private int width=0;
+	private int positionX = 0, positionY = 0;
+	private int width = 0;
 	private Sprite[][] tiles;
 	public static final byte MOVE_COUNTDOWN = 2;
 	private byte moveCountdown = MOVE_COUNTDOWN;
 	public static final int BG_SPRITE = 16;
-	
+
 	public Background(int x, int y, int width, int height) {
-		this.x = x;
-		this.y = y;
+		this.positionX = x;
+		this.positionY = y;
 		this.width = width;
-		if(width%Sprite.SPRITE_WIDTH != 0) {
+		if (width % Sprite.SPRITE_WIDTH != 0) {
 			throw new RuntimeException("Illegal width for Background: " + width);
 		}
-		if(height%Sprite.SPRITE_HEIGHT != 0) {
+		if (height % Sprite.SPRITE_HEIGHT != 0) {
 			throw new RuntimeException("Illegal height for Background: " + height);
 		}
-		tiles = new Sprite[height/Sprite.SPRITE_HEIGHT][width/Sprite.SPRITE_WIDTH+1];
-		for(int i=0; i<width/Sprite.SPRITE_WIDTH+1; i++) {
-			for(int j=0; j<height/Sprite.SPRITE_HEIGHT; j++) {
-				tiles[j][i] = new Sprite(BG_SPRITE, x+(i-1)*Sprite.SPRITE_WIDTH, y+j*Sprite.SPRITE_HEIGHT, false);
+		tiles = new Sprite[height / Sprite.SPRITE_HEIGHT][width / Sprite.SPRITE_WIDTH + 1];
+		for (int i = 0; i < width / Sprite.SPRITE_WIDTH + 1; i++) {
+			for (int j = 0; j < height / Sprite.SPRITE_HEIGHT; j++) {
+				tiles[j][i] = new Sprite(BG_SPRITE, x + (i - 1) * Sprite.SPRITE_WIDTH, y + j * Sprite.SPRITE_HEIGHT,
+						false);
 			}
 		}
 	}
@@ -38,22 +39,22 @@ public class Background implements Renderable, Tickable {
 	}
 
 	@Override
-	public void drawOn(Graphics2D g, int scrollx, int scrolly) {
-		for(Sprite[] ss : tiles) {
-			for(Sprite s : ss) {
-				s.drawOn(g, 0, 0);
+	public void drawOn(Graphics2D graphics, int scrollx, int scrolly) {
+		for (Sprite[] ss : tiles) {
+			for (Sprite s : ss) {
+				s.drawOn(graphics, 0, 0);
 			}
 		}
 	}
 
 	@Override
 	public int getX() {
-		return x;
+		return positionX;
 	}
 
 	@Override
 	public int getY() {
-		return y;
+		return positionY;
 	}
 
 	@Override
@@ -68,13 +69,13 @@ public class Background implements Renderable, Tickable {
 
 	@Override
 	public void goTo(int x, int y) {
-		int dx = x-this.x;
-		int dy = y-this.y;
-		this.x = x;
-		this.y = y;
-		for(Sprite[] ss : tiles) {
-			for(Sprite s : ss) {
-				s.goTo(s.getX()+dx, s.getY()+dy);
+		int dx = x - this.positionX;
+		int dy = y - this.positionY;
+		this.positionX = x;
+		this.positionY = y;
+		for (Sprite[] ss : tiles) {
+			for (Sprite s : ss) {
+				s.goTo(s.getX() + dx, s.getY() + dy);
 			}
 		}
 	}
@@ -82,19 +83,19 @@ public class Background implements Renderable, Tickable {
 	@Override
 	public void tick() {
 		moveCountdown--;
-		if(moveCountdown==0) {
+		if (moveCountdown == 0) {
 			moveCountdown = MOVE_COUNTDOWN;
-			for(int i=0; i<tiles.length; i++) {
-				for(Sprite s : tiles[i]) {
-					if(i%2 == 0) {
-						s.goTo(s.getX()+1, s.getY());
-						if(s.getX() > x+width) {
-							s.goTo(x-Sprite.SPRITE_WIDTH+1, s.getY());
+			for (int i = 0; i < tiles.length; i++) {
+				for (Sprite tiles : tiles[i]) {
+					if (i % 2 == 0) {
+						tiles.goTo(tiles.getX() + 1, tiles.getY());
+						if (tiles.getX() > positionX + width) {
+							tiles.goTo(positionX - Sprite.SPRITE_WIDTH + 1, tiles.getY());
 						}
 					} else {
-						s.goTo(s.getX()-1, s.getY());
-						if(s.getX() < x-Sprite.SPRITE_WIDTH) {
-							s.goTo(x+width-1, s.getY());
+						tiles.goTo(tiles.getX() - 1, tiles.getY());
+						if (tiles.getX() < positionX - Sprite.SPRITE_WIDTH) {
+							tiles.goTo(positionX + width - 1, tiles.getY());
 						}
 					}
 				}
