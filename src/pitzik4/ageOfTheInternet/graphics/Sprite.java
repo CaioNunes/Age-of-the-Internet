8a721b9ccc3ea5cb3792b524863e3029074fa93b
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-11 23:11:11 -0300

    applying style sheet on Sprite.java

--- src/pitzik4/ageOfTheInternet/graphics/Sprite.java
+++ src/pitzik4/ageOfTheInternet/graphics/Sprite.java
@@ -4,46 +4,51 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class Sprite implements Renderable {
-	private int x=0, y=0;
-	private BufferedImage img;
+	private int positionX = 0;
+	private int positionY = 0;
+	private BufferedImage image;
 	public static final int SPRITE_WIDTH = 16;
 	public static final int SPRITE_HEIGHT = 16;
 	public static final int LETTER_WIDTH = 8;
 	public static final int LETTER_HEIGHT = 12;
-	
+
 	public Sprite(int which, int x, int y, BufferedImage spritesheet, int width, int height) {
-		this.x = x;
-		this.y = y;
-		img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g = img.createGraphics();
-		g.drawImage(spritesheet, 0-((which*width)%spritesheet.getWidth()), 0-(((which)/(spritesheet.getWidth()/width))*height), null);
-		g.dispose();
+		this.positionX = x;
+		this.positionY = y;
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = image.createGraphics();
+		graphics.drawImage(spritesheet, 0 - ((which * width) % spritesheet.getWidth()),
+				0 - (((which) / (spritesheet.getWidth() / width)) * height), null);
+		graphics.dispose();
 	}
+
 	public Sprite(int which, int x, int y, boolean letter) {
-		this(which, x, y, (letter?Screen.font:Screen.spritesheet), letter?LETTER_WIDTH:SPRITE_WIDTH, letter?LETTER_HEIGHT:SPRITE_HEIGHT);
+		this(which, x, y, (letter ? Screen.font : Screen.spritesheet), letter ? LETTER_WIDTH : SPRITE_WIDTH,
+				letter ? LETTER_HEIGHT : SPRITE_HEIGHT);
 	}
+
 	public Sprite(int which) {
 		this(which, 0, 0, false);
 	}
 
 	@Override
 	public BufferedImage draw() {
-		return img;
+		return image;
 	}
 
 	@Override
 	public void drawOn(Graphics2D g, int scrollx, int scrolly) {
-		g.drawImage(img, x-scrollx, y-scrolly, null);
+		g.drawImage(image, positionX - scrollx, positionY - scrolly, null);
 	}
 
 	@Override
 	public int getX() {
-		return x;
+		return positionX;
 	}
 
 	@Override
 	public int getY() {
-		return y;
+		return positionY;
 	}
 
 	@Override
@@ -55,15 +60,17 @@ public class Sprite implements Renderable {
 	public int getYOffset() {
 		return 0;
 	}
+
 	@Override
 	public void goTo(int x, int y) {
-		this.x = x;
-		this.y = y;
+		this.positionX = x;
+		this.positionY = y;
 	}
+
 	public void mergeSprite(Sprite spr) {
-		Graphics2D g = img.createGraphics();
-		g.drawImage(spr.draw(), 0, 0, null);
-		g.dispose();
+		Graphics2D graphics = image.createGraphics();
+		graphics.drawImage(spr.draw(), 0, 0, null);
+		graphics.dispose();
 	}
 
 }
