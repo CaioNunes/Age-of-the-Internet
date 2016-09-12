740bd376d36c7c92119282bc1189a3769e8c2e48
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-12 10:14:01 -0300

    applying style sheet on LoseScreen.java

--- src/pitzik4/ageOfTheInternet/LoseScreen.java
+++ src/pitzik4/ageOfTheInternet/LoseScreen.java
@@ -10,18 +10,18 @@ public class LoseScreen implements RenderableTickable {
 	private RenderableString youLose;
 	private RenderableString loseReason;
 	public Button retry;
-	private BlueFrame bg;
-	private int x=0, y=0;
-	
+	private BlueFrame blueFrame_bg;
+	private int positionX = 0, positionY = 0;
+
 	public LoseScreen(Game owner, int x, int y, int width, int height, String reason) {
-		this.x = x;
-		this.y = y;
-		bg = new BlueFrame(x, y, width, height);
+		this.positionX = x;
+		this.positionY = y;
+		blueFrame_bg = new BlueFrame(x, y, width, height);
 		youLose = new RenderableString("Level Unsuccessful", 0, 0);
-		youLose.goTo(x+((width-youLose.width)/2), y+(height/3-youLose.height/2));
+		youLose.goTo(x + ((width - youLose.width) / 2), y + (height / 3 - youLose.height / 2));
 		loseReason = new RenderableString(reason, 0, 0);
-		loseReason.goTo(x+((width-loseReason.width)/2), y+(height/2-loseReason.height/2));
-		retry = new Button(owner, x+width/4, y+(height/3)*2, width/2, "Retry");
+		loseReason.goTo(x + ((width - loseReason.width) / 2), y + (height / 2 - loseReason.height / 2));
+		retry = new Button(owner, x + width / 4, y + (height / 3) * 2, width / 2, "Retry");
 	}
 
 	@Override
@@ -31,21 +31,21 @@ public class LoseScreen implements RenderableTickable {
 	}
 
 	@Override
-	public void drawOn(Graphics2D g, int scrollx, int scrolly) {
-		bg.drawOn(g, scrollx, scrolly);
-		youLose.drawOn(g, scrollx, scrolly);
-		loseReason.drawOn(g, scrollx, scrolly);
-		retry.drawOn(g, scrollx, scrolly);
+	public void drawOn(Graphics2D graphics, int scrollx, int scrolly) {
+		blueFrame_bg.drawOn(graphics, scrollx, scrolly);
+		youLose.drawOn(graphics, scrollx, scrolly);
+		loseReason.drawOn(graphics, scrollx, scrolly);
+		retry.drawOn(graphics, scrollx, scrolly);
 	}
 
 	@Override
 	public int getX() {
-		return x;
+		return positionX;
 	}
 
 	@Override
 	public int getY() {
-		return y;
+		return positionY;
 	}
 
 	@Override
@@ -60,14 +60,14 @@ public class LoseScreen implements RenderableTickable {
 
 	@Override
 	public void goTo(int x, int y) {
-		int dx = x-this.x;
-		int dy = y-this.y;
-		this.x = x;
-		this.y = y;
-		youLose.goTo(youLose.getX()+dx, youLose.getY()+dy);
-		loseReason.goTo(loseReason.getX()+dx, loseReason.getY()+dy);
-		retry.goTo(retry.getX()+dx, retry.getY()+dy);
-		bg.goTo(bg.getX()+dx, bg.getY()+dy);
+		int dx = x - this.positionX;
+		int dy = y - this.positionY;
+		this.positionX = x;
+		this.positionY = y;
+		youLose.goTo(youLose.getX() + dx, youLose.getY() + dy);
+		loseReason.goTo(loseReason.getX() + dx, loseReason.getY() + dy);
+		retry.goTo(retry.getX() + dx, retry.getY() + dy);
+		blueFrame_bg.goTo(blueFrame_bg.getX() + dx, blueFrame_bg.getY() + dy);
 	}
 
 	@Override
