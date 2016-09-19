eb8976bc45284adc0ef5f71fb8e4d7aad203ae24
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-09-18 23:23:34 -0300

    applying style sheet on ComputerTile.java

--- src/pitzik4/ageOfTheInternet/tiles/ComputerTile.java
+++ src/pitzik4/ageOfTheInternet/tiles/ComputerTile.java
@@ -12,95 +12,108 @@ public class ComputerTile extends Tile {
 	private Menu rightClickMenu = null;
 	private boolean yours = false;
 	private boolean theirs = false;
-	public static final int RI_CLI_MENU_WIDTH = 96;
-	public String[] riCliMenuOptions = {"Hack"};
-	public String riCliMenuTitle = "Computer";
-	
+	public static final int RIGHT_CLICK_MENU_WIDTH = 96;
+	public String[] rightClickMenuOptions = { "Hack" };
+	public String rightClickMenuTitle = "Computer";
+
 	public ComputerTile(int x, int y) {
 		this(x, y, Game.game);
 	}
-	public ComputerTile(int x, int y, Game owner) {
-		this.x = x;
-		this.y = y;
+
+	public ComputerTile(int positionX, int positionY, Game owner) {
+		this.x = positionX;
+		this.y = positionY;
 		this.owner = owner;
-		sprite = new Sprite(33, x, y, false);
+		sprite = new Sprite(33, positionX, positionY, false);
 	}
 
 	@Override
 	public void tick() {
-		if(owner.mouseInsideOf(x, y, Sprite.SPRITE_WIDTH, Sprite.SPRITE_HEIGHT)) {
-			if(owner.mouseDown) {
-				if(rightClickMenu == null) {
-					if((((Level) owner.currentLevel).canBeHackedBy(this) != null && ((Level) owner.currentLevel).getRAM() >= hackCost()) || yours) {
-						rightClickMenu = new Menu(owner, x+Sprite.SPRITE_WIDTH, y, RI_CLI_MENU_WIDTH, riCliMenuOptions, riCliMenuTitle);
+		if (owner.mouseInsideOf(x, y, Sprite.SPRITE_WIDTH, Sprite.SPRITE_HEIGHT)) {
+			if (owner.mouseDown) {
+				if (rightClickMenu == null) {
+					if ((((Level) owner.currentLevel).canBeHackedBy(this) != null
+							&& ((Level) owner.currentLevel).getRAM() >= hackCost()) || yours) {
+						rightClickMenu = new Menu(owner, x + Sprite.SPRITE_WIDTH, y, RIGHT_CLICK_MENU_WIDTH,
+								rightClickMenuOptions, rightClickMenuTitle);
 						owner.screen.addRenderable(rightClickMenu);
 					}
 				}
 			}
 		}
-		if(rightClickMenu != null) {
+		if (rightClickMenu != null) {
 			rightClickMenu.tick();
-			if(rightClickMenu.buttons[0].nowClicked) {
-				if(!yours) {
+			if (rightClickMenu.buttons[0].nowClicked) {
+				if (!yours) {
 					hack();
 				} else {
 					unHack();
 				}
 				rightClickMenu.exited = true;
 			}
-			if(rightClickMenu.exited) {
+			if (rightClickMenu.exited) {
 				owner.screen.removeRenderable(rightClickMenu);
 				rightClickMenu = null;
 			}
 		}
 	}
+
 	@Override
-	public void furtherDraw(Graphics2D g, int scrollx, int scrolly) {
-		//if(rightClickMenu != null)
-		//	rightClickMenu.drawOn(g, scrollx, scrolly);
+	public void furtherDraw(Graphics2D graphics, int scrollx, int scrolly) {
+		// if(rightClickMenu != null)
+		// rightClickMenu.drawOn(g, scrollx, scrolly);
 	}
+
 	public void hack() {
-		if(!yours) {
+		if (!yours) {
 			yours = true;
-			//sprite = new Sprite(32, x, y, false);
-			riCliMenuOptions[0] = "Unhack";
+			// sprite = new Sprite(32, x, y, false);
+			rightClickMenuOptions[0] = "Unhack";
 			((Level) owner.currentLevel).hack(this);
 		}
 	}
+
 	@Override
 	public void startEvil() {
 		theirs = true;
 	}
+
 	@Override
 	public void deEvil() {
 		theirs = false;
-		sprite = new Sprite(33, x, y, false);
+		sprite = new Sprite(33, x, y, false); // 33?
 	}
+
 	@Override
 	public void beOwned() {
-		sprite = new Sprite(32, x, y, false);
+		sprite = new Sprite(32, x, y, false); // 32?
 	}
+
 	@Override
 	public void beEvil() {
-		sprite = new Sprite(22, x, y, false);
+		sprite = new Sprite(22, x, y, false); // 22
 	}
+
 	@Override
 	public boolean canBeEvil() {
 		return true;
 	}
+
 	public void unHack() {
-		if(yours) {
+		if (yours) {
 			yours = false;
 			sprite = new Sprite(33, x, y, false);
-			riCliMenuOptions = new String[1];
-			riCliMenuOptions[0] = "Hack";
+			rightClickMenuOptions = new String[1];
+			rightClickMenuOptions[0] = "Hack";
 			((Level) owner.currentLevel).unhack(this);
 		}
 	}
+
 	@Override
 	public int hackCost() {
 		return 5;
 	}
+
 	@Override
 	public boolean isEvil() {
 		return theirs;
