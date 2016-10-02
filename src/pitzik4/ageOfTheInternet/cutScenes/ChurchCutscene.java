adb2cd83e3ea0181137069357bc6711c705a22eb
CaioGabrielAraujo <caiohgabriel@gmail.com>
2016-10-02 18:50:41 -0300

    Applying "Don't trust no one" on ChurchCutscene.java

--- src/pitzik4/ageOfTheInternet/cutScenes/ChurchCutscene.java
+++ src/pitzik4/ageOfTheInternet/cutScenes/ChurchCutscene.java
@@ -22,9 +22,13 @@ public class ChurchCutscene implements Stage {
 	private boolean closing = false;
 	private Game owner;
 	private Set<Integer> lastKeysPressed = new HashSet<Integer>();
-	
+
 	public ChurchCutscene(Game owner) {
-		this.owner = owner;
+		if (owner != null) {
+			this.owner = owner;
+		} else {
+			// nothing to do.
+		}
 	}
 
 	@Override
@@ -40,11 +44,15 @@ public class ChurchCutscene implements Stage {
 	public void drawOn(Graphics2D graphics, int scrollx, int scrolly) {
 		scrollx = 0;
 		scrolly = 0;
-		graphics.drawImage(StartingCutscene.bg, 0, 0, null);
+		if(graphics != null){
+			graphics.drawImage(StartingCutscene.bg, 0, 0, null);
+		}else {
+			//nothing to do
+		}
 		drThompson.drawOn(graphics, 0, 0);
 		screen.drawOn(graphics, 0, 0);
 		digitizer.drawOn(graphics, 0, 0);
-		if(dialogue != null) {
+		if (dialogue != null) {
 			dialogue.drawOn(graphics, 0, 0);
 		}
 	}
@@ -77,77 +85,81 @@ public class ChurchCutscene implements Stage {
 	@Override
 	public void tick() {
 		Set<Integer> keysPressed = new HashSet<Integer>(owner.keysDown);
-		if(dialogue == null) {
+		if (dialogue == null) {
 			lifeTime++;
 		} else {
 			dialogue.tick();
-			if(keysPressed.contains(32) && !lastKeysPressed.contains(32)) {
-				if(dialogue.isGoing()) {
+			if (keysPressed.contains(32) && !lastKeysPressed.contains(32)) {
+				if (dialogue.isGoing()) {
 					dialogue.finish();
 				} else {
 					dialogue = null;
 				}
 			}
 		}
-		if(lifeTime == 10) {
+		if (lifeTime == 10) {
 			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead, "What's new?");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 12) {
-			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.drThompsonHead, "Well, I heard the Church of Pitzik4 has weak Internet security.");
+		} else if (lifeTime == 12) {
+			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.drThompsonHead,
+					"Well, I heard the Church of Pitzik4 has weak Internet security.");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 14) {
-			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead, "I don't believe in Pitzik4. I'll hack them.");
+		} else if (lifeTime == 14) {
+			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead,
+					"I don't believe in Pitzik4. I'll hack them.");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 16) {
+		} else if (lifeTime == 16) {
 			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.drThompsonHead, "That's not very nice.");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 18) {
-			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead, "Don't worry, I have an idea on what to do with them.");
+		} else if (lifeTime == 18) {
+			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead,
+					"Don't worry, I have an idea on what to do with them.");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 20) {
-			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead, "I'll schedule a whole bunch of events so that corporations'");
+		} else if (lifeTime == 20) {
+			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead,
+					"I'll schedule a whole bunch of events so that corporations'");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 22) {
+		} else if (lifeTime == 22) {
 			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead, "counter-hackers are occupied.");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 24) {
+		} else if (lifeTime == 24) {
 			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.drThompsonHead, "That's actually quite clever...");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 26) {
+		} else if (lifeTime == 26) {
 			dialogue = new InfoBox(4, 4, 312, 64, StartingCutscene.tobyHead, "You sound surprised.");
 			dialogue.go();
 			lifeTime++;
-		} else if(lifeTime == 28) {
+		} else if (lifeTime == 28) {
 			done = true;
 		}
-		if(owner.keysDown.contains(10)) {
+		if (owner.keysDown.contains(10)) {
 			done = true;
 		}
 		lastKeysPressed = keysPressed;
-		if(done) {
-			if(owner.screen.getFade() < 255) {
-				if(owner.screen.getFade() > 245) {
+		if (done) {
+			if (owner.screen.getFade() < 255) {
+				if (owner.screen.getFade() > 245) {
 					owner.screen.fadeTo(255);
 				} else {
-					owner.screen.fadeTo(owner.screen.getFade()+10);
+					owner.screen.fadeTo(owner.screen.getFade() + 10);
 				}
 			} else {
 				closing = true;
 			}
 		}
-		if(owner.screen.getFade() > 0 && !done) {
-			if(owner.screen.getFade() < 10) {
+		if (owner.screen.getFade() > 0 && !done) {
+			if (owner.screen.getFade() < 10) {
 				owner.screen.fadeTo(0);
 			} else {
-				owner.screen.fadeTo(owner.screen.getFade()-10);
+				owner.screen.fadeTo(owner.screen.getFade() - 10);
 			}
 		}
 	}
@@ -161,7 +173,8 @@ public class ChurchCutscene implements Stage {
 	public boolean isResetting() {
 		return false;
 	}
-	 // MAGIC NUMBRS
+
+	// MAGIC NUMBRS
 	@Override
 	public int getWidth() {
 		return 320;
