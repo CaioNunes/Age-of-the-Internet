07b9ff6cc19199e0e03cfbd9dba5aef6dee917b0
joaosaliba <joao_salibaa@hotmail.com>
2016-10-13 10:38:23 -0300

    Applying "Don't let the other changing where the should not on HomeTile

--- src/pitzik4/ageOfTheInternet/tiles/HomeTile.java
+++ src/pitzik4/ageOfTheInternet/tiles/HomeTile.java
@@ -12,47 +12,49 @@ public class HomeTile extends Tile {
 	private Menu rightClickMenu = null;
 	private Game owner;
 	public static final int RI_CLI_MENU_WIDTH = 96;
-	public static final String[] RI_CLI_MENU_OPTIONS = {"Upgrade"};
+	public static final String[] RI_CLI_MENU_OPTIONS = { "Upgrade" };
 	public static final String RI_CLI_MENU_TITLE = "Home";
-	
+
 	public HomeTile(int x, int y, Game owner) {
-		this.x = x;
-		this.y = y;
+		setX(x);
+		setY(y);
 		this.owner = owner;
-		sprite = new Sprite(currentAnimation, x, y, false);
+		setSprite(new Sprite(currentAnimation, getX(), getY(), false));
 	}
 
 	@Override
 	public void tick() {
 		animationCountDown--;
-		if(animationCountDown <= 0) {
+		if (animationCountDown <= 0) {
 			animationCountDown = ANIMATION_COUNTDOWN;
 			currentAnimation++;
-			if(currentAnimation > 36) {
+			if (currentAnimation > 36) {
 				currentAnimation = 34;
 			}
-			sprite = new Sprite(currentAnimation, x, y, false);
+			setSprite(new Sprite(currentAnimation, getX(), getY(), false));
 		}
-		if(owner.mouseInsideOf(x, y, Sprite.SPRITE_WIDTH, Sprite.SPRITE_HEIGHT)) {
-			if(owner.mouseDown) {
-				if(rightClickMenu == null && ((Level) owner.currentLevel).canUpgrade()) {
-					rightClickMenu = new Menu(owner, x+Sprite.SPRITE_WIDTH, y, RI_CLI_MENU_WIDTH, RI_CLI_MENU_OPTIONS, RI_CLI_MENU_TITLE);
+		if (owner.mouseInsideOf(getX(), getY(), Sprite.SPRITE_WIDTH, Sprite.SPRITE_HEIGHT)) {
+			if (owner.mouseDown) {
+				if (rightClickMenu == null && ((Level) owner.currentLevel).canUpgrade()) {
+					rightClickMenu = new Menu(owner, getX() + Sprite.SPRITE_WIDTH, getY(), RI_CLI_MENU_WIDTH,
+							RI_CLI_MENU_OPTIONS, RI_CLI_MENU_TITLE);
 					owner.screen.addRenderable(rightClickMenu);
 				}
 			}
 		}
-		if(rightClickMenu != null) {
+		if (rightClickMenu != null) {
 			rightClickMenu.tick();
-			if(rightClickMenu.buttons[0].nowClicked) {
+			if (rightClickMenu.buttons[0].nowClicked) {
 				((Level) owner.currentLevel).upgradeHome();
 				rightClickMenu.exited = true;
 			}
-			if(rightClickMenu.exited) {
+			if (rightClickMenu.exited) {
 				owner.screen.removeRenderable(rightClickMenu);
 				rightClickMenu = null;
 			}
 		}
 	}
+
 	@Override
 	public int hackCost() {
 		return 0;
