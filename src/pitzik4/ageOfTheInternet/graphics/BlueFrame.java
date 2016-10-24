86b2b700c83cf075eb07a0347c10cc5d3abda31a
CaioNunes <caiofelipe147@gmail.com>
2016-10-24 13:38:06 -0200

    Applying " Don't let the other changing where the should not" on BlueFrame class.

--- src/pitzik4/ageOfTheInternet/graphics/BlueFrame.java
+++ src/pitzik4/ageOfTheInternet/graphics/BlueFrame.java
@@ -7,48 +7,89 @@ public class BlueFrame implements Renderable {
 	private static final int[][] BG_SPRITES = { { 192, 193, 194 }, { 224, 225, 226 }, { 256, 257, 258 } };
 	private static final int BG_SPRITE_SIZE = 8;
 	private Sprite[][] background;
-	private int width, height;
-	private int positionX, positionY;
+	private int width = 0;
+	private int height = 0;
+	private int positionX = 0;
+	private int positionY = 0;
 
 	public BlueFrame(int x, int y, int width, int height) {
-		this.positionX = x;
-		this.positionY = y;
-		this.width = width;
-		this.height = height;
-		background = new Sprite[width / BG_SPRITE_SIZE][height / BG_SPRITE_SIZE];
+		setPositionX(x);
+		setPositionY(y);
+		setWidth(width);
+		setHeight(height);
+		Sprite[][] background = new Sprite[width / getBgSpriteSize()][height / getBgSpriteSize()]; 
+		setBackground(background);
+		
 		assert(background != null): "background not instantiated";
-		for (int i = 0; i < width; i += BG_SPRITE_SIZE) {
-			for (int j = 0; j < height; j += BG_SPRITE_SIZE) {
+		
+		for (int i = 0; i < width; i += getBgSpriteSize()) {
+			for (int j = 0; j < height; j += getBgSpriteSize()) {
 				int whichx;
 				int whichy;
 				if (i == 0) {
 					whichy = 0;
-				} else if (i == width - BG_SPRITE_SIZE) {
+				} else if (i == width - getBgSpriteSize()) {
 					whichy = 2;
 				} else {
 					whichy = 1;
 				}
 				if (j == 0) {
 					whichx = 0;
-				} else if (j == height - BG_SPRITE_SIZE) {
+				} else if (j == height - getBgSpriteSize()) {
 					whichx = 2;
 				} else {
 					whichx = 1;
 				}
-				background[i / BG_SPRITE_SIZE][j / BG_SPRITE_SIZE] = new Sprite(BG_SPRITES[whichx][whichy], i + x,
-						j + x, Screen.spritesheet, BG_SPRITE_SIZE, BG_SPRITE_SIZE);
+		
+				background[i / getBgSpriteSize()][j / getBgSpriteSize()] = new Sprite(getBgSprites()[whichx][whichy], i + x,
+						j + x, Screen.spritesheet, getBgSpriteSize(), getBgSpriteSize());
 			}
 		}
 	}
 
+	public Sprite[][] getBackground() {
+		return background;
+	}
+
+	public void setBackground(Sprite[][] background) {
+		this.background = background;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public static int[][] getBgSprites() {
+		return BG_SPRITES;
+	}
+
+	public static int getBgSpriteSize() {
+		return BG_SPRITE_SIZE;
+	}
+
 	@Override
 	public BufferedImage draw() {
-		BufferedImage bufferedImageOut = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage bufferedImageOut = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 		assert(bufferedImageOut != null): "BufferedImageOut not instantiated";
+		
 		Graphics2D graphics = bufferedImageOut.createGraphics();
 		assert(graphics != null): "graphics not created";
-		drawOn(graphics, positionX, positionY);
+		
+		drawOn(graphics, getPositionX(), getPositionY());
 		graphics.dispose();
+		
 		assert(bufferedImageOut != null): "Return BufferedImageOut is null";
 		return bufferedImageOut;
 	}
@@ -56,8 +97,8 @@ public class BlueFrame implements Renderable {
 	@Override
 	public void drawOn(Graphics2D g, int scrollx, int scrolly) {
 		assert(g != null): "Graphics2D-g parameter is null";
-		assert(background != null): "Background var is null";
-		for (Sprite[] ss : background) {
+		assert(getBackground() != null): "Background var is null";
+		for (Sprite[] ss : getBackground()) {
 			assert(ss != null): "ss var is null";
 			for (Sprite s : ss) {
 				assert(s != null): "s var is null";
@@ -69,6 +110,22 @@ public class BlueFrame implements Renderable {
 	@Override
 	public int getX() {
 		return positionX;
+	}
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
 	}
 
 	@Override
@@ -88,12 +145,12 @@ public class BlueFrame implements Renderable {
 
 	@Override
 	public void goTo(int x, int y) {
-		int dx = x - this.positionX;
-		int dy = y - this.positionY;
-		this.positionX = x;
-		this.positionY = y;
-		assert(background != null): "Background var is null";
-		for (Sprite[] ss : background) {
+		int dx = x - getPositionX();
+		int dy = y - getPositionY();
+		setPositionX(x);
+		setPositionY(y);
+		assert(getBackground() != null): "Background var is null";
+		for (Sprite[] ss : getBackground()) {
 			assert(ss != null): "ss var is null";
 			for (Sprite s : ss) {
 				assert(s != null): "s var is null";
