35b4b1e3cf952431c747fdeb23fb4eb9144e221e
Alexandre TK <alexandrekry@gmail.com>
2016-11-23 19:57:40 -0200

    paragraphs of code - src/pitzik4/ageOfTheInternet/cutScenes/ChurchCutscene.java

--- src/pitzik4/ageOfTheInternet/cutScenes/ChurchCutscene.java
+++ src/pitzik4/ageOfTheInternet/cutScenes/ChurchCutscene.java
@@ -30,6 +30,7 @@ public class ChurchCutscene implements Stage {
 	private static final int DEFAULT_WIDTH = 320;
 	private static final int DEFAULT_HEIGHT = 240;
 
+	
 	public ChurchCutscene(Game owner) {
 		if (owner != null) {
 			this.owner = owner;
@@ -38,6 +39,7 @@ public class ChurchCutscene implements Stage {
 		}
 	}
 
+	
 	@Override
 	public BufferedImage draw() {
 		BufferedImage out = new BufferedImage(DEFAULT_WIDTH, DEFAULT_HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -47,8 +49,11 @@ public class ChurchCutscene implements Stage {
 		return null;
 	}
 
+	
 	@Override
 	public void drawOn(Graphics2D graphics, int scrollx, int scrolly) {
+		
+		// Draw the cutscene background.
 		scrollx = 0;
 		scrolly = 0;
 		if (graphics != null) {
@@ -56,6 +61,8 @@ public class ChurchCutscene implements Stage {
 		} else {
 			// do nothing
 		}
+		
+		// Draw the cutscene elements, like the characters, screen...
 		drThompson.drawOn(graphics, 0, 0);
 		screen.drawOn(graphics, 0, 0);
 		digitizer.drawOn(graphics, 0, 0);
@@ -67,36 +74,47 @@ public class ChurchCutscene implements Stage {
 
 	}
 
+	
 	@Override
 	public int getX() {
 		return 0;
 	}
 
+	
 	@Override
 	public int getY() {
 		return 0;
 	}
 
+	
 	@Override
 	public int getXOffset() {
 		return 0;
 	}
 
+	
 	@Override
 	public int getYOffset() {
 		return 0;
 	}
 
+	
 	@Override
 	public void goTo(int walkPositionX, int walkPositionY) {
 
 	}
 
+	
 	@Override
 	public void tick() {
+		
 		Set<Integer> keysPressed = new HashSet<Integer>(owner.keysDown);
+		
+		// If finished the dialog, go to the next "tick".
 		if (dialogue == null) {
 			lifeTime++;
+			
+		// If space key is pressed, go to the next dialog.
 		} else {
 			dialogue.tick();
 			final int space_key = 32;
@@ -109,11 +127,16 @@ public class ChurchCutscene implements Stage {
 				}
 			}
 		}
+		
+		// Dialog box characteristics
 		final int box_diagonal_position = 4;
 		final int text_y_position = 4;
 		final int box_width = 312; // >=200
 		final int box_height = 64;
+		
 		switch (lifeTime) {
+		
+		// Scene number 1.
 		case 10:
 			final String dialog_text_10 = "What's new?";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -121,6 +144,8 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
+		
+		// Scene number 2.
 		case 12:
 			final String dialog_text_12 = "Well, I heard the Church of Pitzik4 has weak Internet security.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -128,6 +153,8 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
+		
+		// Scene number 3
 		case 14:
 			final String dialog_text_14 = "I don't believe in Pitzik4. I'll hack them.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -135,6 +162,8 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
+		
+		// Scene number 4
 		case 16:
 			final String dialog_text_16 = "That's not very nice.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -143,6 +172,7 @@ public class ChurchCutscene implements Stage {
 			lifeTime++;
 			break;
 
+		// Scene number 5
 		case 18:
 			final String dialog_text_18 = "Don't worry, I have an idea on what to do with them.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -150,7 +180,8 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
-
+			
+		// Scene number 6
 		case 20:
 			final String dialog_text_20 = "I'll schedule a whole bunch of events so that corporations'";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -158,7 +189,8 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
-
+			
+		// Scene number 7
 		case 22:
 			final String dialog_text_22 = "counter-hackers are occupied.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -166,6 +198,8 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
+			
+		// Scene number 8
 		case 24:
 			final String dialog_text_24 = "That's actually quite clever...";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -173,6 +207,8 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
+
+		// Scene number 9
 		case 26:
 			final String dialog_text_26 = "You sound surprised.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
@@ -180,15 +216,20 @@ public class ChurchCutscene implements Stage {
 			dialogue.go();
 			lifeTime++;
 			break;
+
+		// Scene number 10
 		case 28:
 			done = true;
 			break;
 		}
 
+		// If user is pressing the enter key, jump Cutscene. That is, jump all scenes.
 		final int enter_key = 10;
 		if (owner.keysDown.contains(enter_key)) {
 			done = true;
 		}
+		
+		// If the Cutscene ended, make the screen black.
 		lastKeysPressed = keysPressed;
 		if (done) {
 			final int black_color = 255;
@@ -203,6 +244,8 @@ public class ChurchCutscene implements Stage {
 				closing = true;
 			}
 		}
+		
+		// After making the screen black, make it clear again. 
 		final int clear_color = 0;
 		if (owner.screen.getFade() > clear_color && !done) {
 			if (owner.screen.getFade() < clear_color + 10) {
@@ -212,34 +255,41 @@ public class ChurchCutscene implements Stage {
 				owner.screen.fadeTo(owner.screen.getFade() - color_value_faded_by_tick);
 			}
 		}
+		
 	}
 
+	
 	@Override
 	public boolean isClosing() {
 		return closing;
 	}
 
+	
 	@Override
 	public boolean isResetting() {
 		return false;
 	}
 
+	
 	@Override
 	public int getWidth() {
 		return DEFAULT_WIDTH;
 	}
 
+	
 	@Override
 	public int getHeight() {
 		return DEFAULT_HEIGHT;
 	}
 
+	
 	@Override
 	public Dimension getSize() {
 		final Dimension dimension = new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 		return dimension;
 	}
 
+	
 	@Override
 	public boolean isScrollable() {
 		return false;
