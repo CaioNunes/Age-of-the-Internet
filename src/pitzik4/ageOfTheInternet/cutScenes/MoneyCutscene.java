e4d269c4fbef10e2bd287585fa758c737e88aa27
Alexandre TK <alexandrekry@gmail.com>
2016-11-23 20:11:16 -0200

    paragraphs of code - src/pitzik4/ageOfTheInternet/cutScenes/MoneyCutscene.java

--- src/pitzik4/ageOfTheInternet/cutScenes/MoneyCutscene.java
+++ src/pitzik4/ageOfTheInternet/cutScenes/MoneyCutscene.java
@@ -34,9 +34,9 @@ public class MoneyCutscene implements Stage {
 		} else {
 			// nothing to do.
 		}
-		;
 	}
 
+	
 	@Override
 	public BufferedImage draw() {
 		BufferedImage out = new BufferedImage(DEFAULT_WIDTH, DEFAULT_HEIGHT, BufferedImage.TYPE_INT_RGB);
@@ -46,51 +46,68 @@ public class MoneyCutscene implements Stage {
 		return null;
 	}
 
+	
 	@Override
 	public void drawOn(Graphics2D g, int scrollX, int scrollY) {
+		
+		// Draw the cutscene background.
 		scrollX = 0;
 		scrollY = 0;
 		g.drawImage(StartingCutscene.bg, 0, 0, null);
+		
+		// Draw the cutscene elements, like the characters, screen...
 		drThompson.drawOn(g, 0, 0);
 		screen.drawOn(g, 0, 0);
 		digitizer.drawOn(g, 0, 0);
 		if (dialogue != null) {
 			dialogue.drawOn(g, 0, 0);
+		} else {
+			// DO NOTHING
 		}
 	}
 
+	
 	@Override
 	public int getX() {
 		return 0;
 	}
 
+	
 	@Override
 	public int getY() {
 		return 0;
 	}
 
+	
 	@Override
 	public int getXOffset() {
 		return 0;
 	}
 
+	
 	@Override
 	public int getYOffset() {
 		return 0;
 	}
 
+	
 	@Override
 	public void goTo(int walkPositionX, int walkPositionY) {
 
 	}
 
+	
 	@Override
 	public void tick() {
 		final int SPACEBAR_BUTTON = 32;
 		final int ENTER_BUTTON = 10;
 		Set<Integer> keysPressed = new HashSet<Integer>(owner.keysDown);
+		
+		// If finished the dialog, go to the next "tick".
 		if (dialogue == null) {
 			lifeTime++;
+			
+		// If space key is pressed, go to the next dialog.
 		} else {
 			dialogue.tick();
 			final boolean spacebar_pressed = keysPressed.contains(SPACEBAR_BUTTON)
@@ -103,70 +120,96 @@ public class MoneyCutscene implements Stage {
 				}
 			}
 		}
+		
+		// Dialog box characteristics
 		final int box_diagonal_position = 4;
 		final int text_y_position = 4;
 		final int box_width = 312; // >=200
 		final int box_height = 64;
+		
+		// Scene number 1.
 		if (lifeTime == 10) {
 			final String dialog_text = "Alright, the institution has decided to start funding you.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.drThompsonHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+		
+		// Scene number 2.
 		} else if (lifeTime == 12) {
 			final String dialog_text = "Yay! I get money!";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.tobyHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+			
+		// Scene number 3.
 		} else if (lifeTime == 14) {
 			final String dialog_text = "...Yes. Yes, you get money. Use it to upgrade the computer.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.drThompsonHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+		
+		// Scene number 4.
 		} else if (lifeTime == 16) {
 			final String dialog_text = "You can also steal money from corporations by hacking them.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.drThompsonHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+			
+		// Scene number 5.
 		} else if (lifeTime == 18) {
 			final String dialog_text = "First hacking... Now stealing money! This just gets better and better!";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.tobyHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+			
+		// Scene number 6.
 		} else if (lifeTime == 20) {
 			final String dialog_text = "*sigh* Look out, though, corporations will un-hack their computers.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.drThompsonHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+			
+		// Scene number 7.
 		} else if (lifeTime == 22) {
 			final String dialog_text = "And if you get disconnected from any of your instances, very bad.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.drThompsonHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+			
+		// Scene number 8.
 		} else if (lifeTime == 24) {
 			final String dialog_text = "So, I lose if I get disconnected.";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.tobyHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+			
+		// Scene number 9.
 		} else if (lifeTime == 26) {
 			final String dialog_text = "What do you think this is, a game?";
 			dialogue = new InfoBox(box_diagonal_position, text_y_position, box_width, box_height,
 					StartingCutscene.drThompsonHead, dialog_text);
 			dialogue.go();
 			lifeTime++;
+			
+		// Scene number 10.
 		} else if (lifeTime == 28) {
 			done = true;
 		}
+		
+		// If user is pressing the enter key, jump Cutscene. That is, jump all scenes.
 		if (owner.keysDown.contains(ENTER_BUTTON)) {
 			done = true;
 		}
+		
+		// If the Cutscene ended, make the screen black.
 		lastKeysPressed = keysPressed;
 		if (done) {
 			final int black_color = 255;
@@ -181,6 +224,8 @@ public class MoneyCutscene implements Stage {
 				closing = true;
 			}
 		}
+		
+		// After making the screen black, make it clear again. 
 		final int clear_color = 0;
 		if (owner.screen.getFade() > clear_color && !done) {
 			if (owner.screen.getFade() < clear_color + 10) {
@@ -192,32 +237,37 @@ public class MoneyCutscene implements Stage {
 		}
 	}
 
+	
 	@Override
 	public boolean isClosing() {
 		return closing;
 	}
 
+	
 	@Override
 	public boolean isResetting() {
 		return false;
 	}
 
-	// MAGIC NUMBRS
+	
 	@Override
 	public int getWidth() {
 		return DEFAULT_WIDTH;
 	}
 
+	
 	@Override
 	public int getHeight() {
 		return DEFAULT_HEIGHT;
 	}
 
+	
 	@Override
 	public Dimension getSize() {
 		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 	}
 
+	
 	@Override
 	public boolean isScrollable() {
 		return false;
